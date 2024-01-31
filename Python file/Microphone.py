58fb3b0fb7a6bc580d740ad78b4f3e997fbaf754
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-01-31 15:14:28 -0500

    Added try and except

--- Python file/Microphone.py
+++ Python file/Microphone.py
@@ -8,9 +8,15 @@ mic = sr.Microphone()
 
 # for name in mic.list_microphone_names():
 # 	print(name)
+try:
+	with mic as source:
+		# r.adjust_for_ambient_noise(source)
+		audio = r.listen(source)
 
-with mic as source:
-	# r.adjust_for_ambient_noise(source)
-	audio = r.listen(source)
+	print(r.recognize_google(audio))  # Sentence is fine but not when it comes to name
 
-print(r.recognize_google(audio)) # Sentence is fine but not when it comes to name
+except sr.UnknownValueError:
+	print("Speech Recognition could not understand audio")
+except Exception as e:
+	print(f"An unexpected error occurred: {e}")
+
