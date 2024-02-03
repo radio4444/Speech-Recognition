17b3eaa89d3c55ed05ad8ce8fd4db62d3507baac
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-02-03 18:16:16 -0500

    Added RecordingTextFile.py, it has both function: create_directory and stt_file. And moved couple file for tardiness

--- Python file/Microphone.py
+++ Python file/Microphone.py
@@ -1,31 +1,9 @@
 import speech_recognition as sr
-import os
-
-
-# Create new directory and save the file name
-def create_directory(directory_name):
-	current_directory = os.getcwd()
-	# One level up the current directory and create new directory
-	directory_path = os.path.join(current_directory, '..', directory_name)
-	if not os.path.exists(directory_path):  # if the directory does not exist, create it
-		os.makedirs(directory_path)
-
-	return directory_path
-
-
-# Store the recording speech to text file
-def STT_file(file_name, mic_recording):
-	directory_name = "Speech to text"
-	directory_path = create_directory(directory_name)
-	file_path = os.path.join(directory_path, file_name)
-	with open('{}.txt'.format(file_path), 'a') as text_file:  # Create new and save the file
-		text_file.write(mic_recording)
-	with open('{}.txt'.format(file_path), 'r') as read_text_file:  # Output the file that had just been saved
-		print(read_text_file.read())
-
+from RecordingTextFile import RecordingTextFile
 
 r = sr.Recognizer()
 mic = sr.Microphone()
+recording_file_object = RecordingTextFile()
 user_file_name = input("Enter the file name: ")
 try:
 	# Listen to the user through the mic
@@ -33,7 +11,8 @@ try:
 	with mic as source:
 		audio = r.listen(source)
 	# Save recording in text format file
-	STT_file(user_file_name, r.recognize_google(audio))
+
+	recording_file_object.stt_file(user_file_name, r.recognize_google(audio))
 except sr.UnknownValueError:  # if the mic is not picking up any voice
 	print("Speech Recognition could not understand audio")
 except Exception as e:
---
+++ Python file/RecordingTextFile.py
@@ -0,0 +1,28 @@
+import os
+
+
+class RecordingTextFile:
+
+	# Constructor method
+	def __init__(self, directory_name='speech to text'):
+		self.directory_name = directory_name
+
+	# Create new directory
+
+	def create_directory(self):
+		current_directory = os.getcwd()
+		# One level up the current directory and create new directory
+		directory_path = os.path.join(current_directory, '..', self.directory_name)
+		if not os.path.exists(directory_path):  # if the directory does not exist, create it
+			os.makedirs(directory_path)
+
+		return directory_path
+
+	# Store the recording speech to text file and save it
+	def stt_file(self, file_name, mic_recording):
+		directory_path = self.create_directory()
+		file_path = os.path.join(directory_path, file_name)
+		with open('{}.txt'.format(file_path), 'a') as text_file:  # Create new and save the file
+			text_file.write(mic_recording)
+		with open('{}.txt'.format(file_path), 'r') as read_text_file:  # Output the file that had just been saved
+			print(read_text_file.read())
