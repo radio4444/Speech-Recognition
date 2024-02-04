d57d06973267c8a374bcec3b7bafb28f4893e010
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-02-04 16:04:37 -0500

    Enhance voice_prompt() to provide continuous user interaction on unclear prompts

--- Python file/Microphone.py
+++ Python file/Microphone.py
@@ -7,14 +7,16 @@ def voice_prompt():
 	mic = sr.Microphone()
 	# Listen to the user through the mic
 	print("Now it is listening to your voice....")
-	try:
-		with mic as source:
-			audio = r.listen(source)
-		return r.recognize_google(audio)
-	except sr.UnknownValueError:  # if the mic is not picking up any voice
-		print("Speech Recognition could not understand audio")
-	except Exception as e:
-		print(f"An unexpected error occurred: {e}")
+	# keep looping till it can return.
+	while True:
+		try:
+			with mic as source:
+				audio = r.listen(source)
+			return r.recognize_google(audio)  # act as break for while loop
+		except sr.UnknownValueError:  # if the mic is not picking up any voice
+			print("Speech Recognition could not understand. Try saying it again")
+		except Exception as e:
+			print(f"An unexpected error occurred: {e}")
 
 
 # Close the program via voice-controlled
