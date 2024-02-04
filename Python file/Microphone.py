7111645f5092af712ed31c6a7e9b4c8072680040
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-02-04 15:09:10 -0500

    Optimize the code. Removed repeated code and turn into re-usable function: voice_prompt()

--- Python file/Microphone.py
+++ Python file/Microphone.py
@@ -2,34 +2,27 @@ import speech_recognition as sr
 from RecordingTextFile import RecordingTextFile
 
 
-def rstt():
-	user_file_name = input("Enter the file name: ")
+def voice_prompt():
+	r = sr.Recognizer()
+	mic = sr.Microphone()
 	# Listen to the user through the mic
 	print("Now it is listening to your voice....")
 	try:
 		with mic as source:
 			audio = r.listen(source)
-		# Save recording in text format file
-		RecordingTextFile(user_file_name, r.recognize_google(audio))
+		return r.recognize_google(audio)
 	except sr.UnknownValueError:  # if the mic is not picking up any voice
 		print("Speech Recognition could not understand audio")
 	except Exception as e:
 		print(f"An unexpected error occurred: {e}")
 
 
-r = sr.Recognizer()
-mic = sr.Microphone()
-# Listen to user and do the prompt the user tell to do
-try:
-	print("Say: Create file")
-	with mic as voice:
-		response = r.listen(voice)
-	response_recognize = r.recognize_google(response)
-	if response_recognize == 'create file':
-		rstt()
-except sr.UnknownValueError:
-	print("The mic is picking up your voice")
-except Exception as a:
-	print(f"This is an unexpected error {a}")
-
+print("Say: Create file")
+response = voice_prompt()
+if response == 'create file':
+	user_filename = input("Enter file name: ")
+	# Save recording in text format file
+	RecordingTextFile(user_filename, voice_prompt())
+else:
+	print(f"This is what you said: {response}")
 input("Press Enter to exit...")
