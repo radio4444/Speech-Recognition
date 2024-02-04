d642e07e1bc0fd51c0e36bb980edd8ca2f8d7631
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-02-04 13:27:55 -0500

    Refactor voice recording and stt conversion Explanation: Extracted the voice recording and stt logic into seperate named 'rstt' Also let the user choose to call the method

--- Python file/Microphone.py
+++ Python file/Microphone.py
@@ -1,19 +1,26 @@
 import speech_recognition as sr
 from RecordingTextFile import RecordingTextFile
 
+
+def rstt():
+	user_file_name = input("Enter the file name: ")
+	# Listen to the user through the mic
+	print("Now it is listening to your voice....")
+	try:
+		with mic as source:
+			audio = r.listen(source)
+		# Save recording in text format file
+		RecordingTextFile(user_file_name, r.recognize_google(audio))
+	except sr.UnknownValueError:  # if the mic is not picking up any voice
+		print("Speech Recognition could not understand audio")
+	except Exception as e:
+		print(f"An unexpected error occurred: {e}")
+
+
 r = sr.Recognizer()
 mic = sr.Microphone()
 
-user_file_name = input("Enter the file name: ")
-try:
-	# Listen to the user through the mic
-	print("Now it is listening to your voice....")
-	with mic as source:
-		audio = r.listen(source)
-	# Save recording in text format file
-	recording_file_object = RecordingTextFile(user_file_name, r.recognize_google(audio))
-except sr.UnknownValueError:  # if the mic is not picking up any voice
-	print("Speech Recognition could not understand audio")
-except Exception as e:
-	print(f"An unexpected error occurred: {e}")
+user_option = input("Enter your choice: 1. Record my speech and store it: ")
+if user_option == '1':
+	rstt()
 input("Press Enter to exit...")
