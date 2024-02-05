e67f245f540d7c5f8f132e36ea54f318b2168db5
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-02-05 16:04:28 -0500

    User refresh to get new urls to choose from.

--- Python file/search_via_web.py
+++ Python file/search_via_web.py
@@ -7,10 +7,17 @@ class Information:
 		self.words = words
 
 	def search_web(self):
-		urls = []
-		for url in search(self.words, stop=4):
-			urls.append(url)
-		print('\n'.join(urls))
-		user_input = int(input("Specify the link via number: "))
-		webbrowser.open(urls[user_input])
-
+		new_results = 0
+		# Keep looping until the user input number for the url
+		while True:
+			urls = []
+			# maybe be turn this into function
+			for url in search(self.words, start=new_results, stop=4):
+				urls.append(url)  # store 4 urls via search method
+			print('\n'.join(urls))  # show the 4 url
+			user_input = input("Specify the link via number 0-3 or refresh: ")
+			new_results = new_results + 4
+			if user_input != 'refresh':
+				# Open the url, the user has specified
+				webbrowser.open(urls[int(user_input)])
+				break
