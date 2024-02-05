b82fbf0378b8666559fb3cf1a8a8e75628ea14a7
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-02-05 16:17:42 -0500

    User can go back four previous urls. And user can exit out.

--- Python file/search_via_web.py
+++ Python file/search_via_web.py
@@ -15,9 +15,20 @@ class Information:
 			for url in search(self.words, start=new_results, stop=4):
 				urls.append(url)  # store 4 urls via search method
 			print('\n'.join(urls))  # show the 4 url
-			user_input = input("Specify the link via number 0-3 or refresh: ")
-			new_results = new_results + 4
-			if user_input != 'refresh':
+			user_input = input("Specify the link via number 0-3 or refresh or previous or exit: ")
+			# give new 4 urls
+			if user_input == 'refresh':
+				new_results = new_results + 4
+			# show previous 4 urls
+			elif user_input == 'previous':
+				new_results = new_results - 4
+				if new_results < 0:
+					print("You cannot go back anymore. These are the first 4 urls")  # maybe remove the extra statement
+			elif user_input == 'exit':
+				break
+			else:
 				# Open the url, the user has specified
 				webbrowser.open(urls[int(user_input)])
 				break
+
+
