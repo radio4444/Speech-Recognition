3ee3f862c51fd9c24308b1b2c193c9bfc9e60ab0
Tanzil[pc] <46586118+radio4444@users.noreply.github.com>
2024-02-06 12:21:47 -0500

    Seperated search_web function into 2 functions: generate_urls and navigate_urls

--- Python file/search_via_web.py
+++ Python file/search_via_web.py
@@ -1,40 +1,51 @@
 from googlesearch import search
 import webbrowser
-import testingMic
 
 
 class Information:
 	def __init__(self, words):
 		self.words = words
+		self.new_results = 0
+		self.urls = []
 
-	# self.search_this = response
+	# Generate 4 new urls using the user phrase
+	def generate_urls(self):
+		self.urls = []
+		for url in search(self.words, start=self.new_results, stop=4):
+			self.urls.append(url)  # store 4 urls via search method
+		return self.urls  # show the 4 url
 
-	def search_web(self):
-		new_results = 0
+	# Either generate new or go back to 4 urls. Or exit out. Or open up the link by indicating number.
+	def navigate_urls(self):
+
 		# Keep looping until the user input number for the url
 		while True:
-			urls = []
-			for url in search(self.words, start=new_results, stop=4):
-				urls.append(url)  # store 4 urls via search method
-			print('\n'.join(urls))  # show the 4 url
-			print("Specify the link via number 0-3 or next or previous or exit: ")
-			user_input = testingMic.voice_prompt()
+			# print 4 new urls
+			print('\n'.join(self.urls))
+			print("Specify the link via number 1-4 or next or previous or exit: ")
+			user_input = input()  # change to response
 
 			try:
 				# give new 4 urls
 				if user_input == 'next':
-					new_results = new_results + 4  # increment start value in search function by 4
+					# increment start value in search function by 4
+					self.new_results += 4
+					# calling this method updated start value
+					self.generate_urls()
 				# show previous 4 urls
 				elif user_input == 'previous':
-					new_results = new_results - 4  # decrement start value in search function by 4
-					if new_results < 0:
+					# decrement start value in search function by 4
+					self.new_results -= 4
+					if self.new_results < 0:
 						print(
 							"You cannot go back anymore. These are the first 4 urls")  # maybe remove the extra statement
+					# calling this method updated start value
+					self.generate_urls()
 				elif user_input == 'exit':
 					break
 				else:
 					# Open the url, the user has specified
-					webbrowser.open(urls[int(user_input)])
+					webbrowser.open(self.urls[int(user_input) - 1])
 					break
 			except Exception as e:
 				print("Invalid input. Please choose the correct response")
